ccf94696ab192ab1383a52765a3d5de16380ee98
SiDzej <Inaros2010@gmail.com>
2014-05-29 21:23:58 +0200

    permission fix

--- src/main/java/eu/sidzej/wc/listeners/transaction/TransactionPreparePerms.java
+++ src/main/java/eu/sidzej/wc/listeners/transaction/TransactionPreparePerms.java
@@ -4,15 +4,20 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
+import eu.sidzej.wc.WCSign.e_type;
 import eu.sidzej.wc.events.TransactionPrepareEvent;
 import eu.sidzej.wc.events.TransactionPrepareEvent.e_states;
 
 public class TransactionPreparePerms implements Listener {
 	
-	@EventHandler(priority = EventPriority.LOW)
+	@EventHandler(priority = EventPriority.HIGH)
 	public void CheckPermission(TransactionPrepareEvent e){
-		if(!e.getPlayer().hasPermission("woodcurrency.buysell"))
-			e.setState(e_states.NO_PERMISSIONS);
+		if(e.getType().equals(e_type.BUY))
+			if(!e.getPlayer().hasPermission("woodcurrency.buy"))
+				e.setState(e_states.NO_PERMISSIONS);
+		if(e.getType().equals(e_type.SELL))
+			if(!e.getPlayer().hasPermission("woodcurrency.sell"))
+				e.setState(e_states.NO_PERMISSIONS);
 	}
 
 }
