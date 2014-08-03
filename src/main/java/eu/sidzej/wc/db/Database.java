474b2f76874b8bcbb9005962c1503a275ae09688
_CJ_ <inaros2010@gmail.com>
2014-08-03 14:09:49 +0200

    DB version 2 - add nick coll

--- src/main/java/eu/sidzej/wc/db/Database.java
+++ src/main/java/eu/sidzej/wc/db/Database.java
@@ -2,17 +2,15 @@ package eu.sidzej.wc.db;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
-import javax.xml.bind.DatatypeConverter;
+import org.bukkit.Bukkit;
 
 import com.google.common.io.CharStreams;
 
@@ -26,77 +24,43 @@ public class Database {
 	private static ConnectionManager cm;
 	public boolean valid = false;
 
-	private List<String> table_names = Arrays.asList("wc_players", "wc_transactions",
-			"wc_signs");
+	public byte current_version = 2;
+
+	private List<String> table_names = Arrays.asList("wc_players",
+			"wc_transactions", "wc_signs", "wc_info");
 
 	public Database(WoodCurrency plugin) throws ClassNotFoundException {
 		this.plugin = plugin;
 
-		cm = new ConnectionManager(Config.host, Config.port, Config.database, Config.user,
-				Config.pass);
+		cm = new ConnectionManager(Config.host, Config.port, Config.database,
+				Config.user, Config.pass);
 		TimedConnection c = cm.getConnection();
-		if(c==null || !c.isValid())
+		if (c == null || !c.isValid())
 			return;
 		c.release();
-		
+
 		Iterator<String> iterator = table_names.iterator();
 		while (iterator.hasNext())
 			if (!tableExists(iterator.next())) {
-				if(!createDBTables())
+				if (!createDBTables())
 					return;
 				break;
 			}
-		
+
 		valid = true;
+		update();
 	}
-	
-	public static TimedConnection getConnection(){
+
+	public static TimedConnection getConnection() {
 		return cm.getConnection();
 	}
 
-	public boolean setPassword(String p, String pass) {
-		TimedConnection c = null;
-		Statement s = null;
-		String password = null;
-		try {
-			MessageDigest md = MessageDigest.getInstance("MD5");
-			md.update(pass.getBytes("UTF-8"));
-			byte[] digest = md.digest();
-			password = DatatypeConverter.printHexBinary(digest);
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		try {
-			c = cm.getConnection();
-			s = c.createStatement();
-			s.execute("INSERT INTO ma_players (nick,password) VALUES (\"" + p + "\",\""
-					+ password + "\") ON DUPLICATE KEY UPDATE password=\""+password+"\"");
-			Log.debug("Setting " + p + " password");
-			
-		} catch (SQLException e) {
-			Log.error(e.getMessage());
-			Log.error("Unable to set password.");
-			return false;
-		} finally {
-			try {
-				if (s != null)
-					s.close();
-				c.release();
-			} catch (SQLException e) {
-				Log.error("Unable to close connection.");
-			}
-		}
-		return true;
-	}
-	
 	public void close() {
-		if(valid)
+		if (valid)
 			cm.close();
 	}
-	
-	/// Private part
+
+	// / Private part
 	private boolean createDBTables() {
 		Log.info("Creating DB tables structure...");
 		TimedConnection c = null;
@@ -109,7 +73,8 @@ public class Database {
 				return false;
 			}
 			String[] queries = CharStreams.toString(
-					new InputStreamReader(plugin.getResource("CREATE.sql"))).split(";");
+					new InputStreamReader(plugin.getResource("CREATE.sql")))
+					.split(";");
 			for (String query : queries) {
 				Log.debug("Creating DB table");
 				s.execute(query);
@@ -130,18 +95,17 @@ public class Database {
 		return true;
 	}
 
-	
 	private boolean tableExists(String table) {
 		TimedConnection c = null;
 		Statement s = null;
 		try {
 			c = cm.getConnection();
 			s = c.createStatement();
-			
+
 			ResultSet res = c.getMetaData().getTables(null, null, table, null);
 			if (res.next())
 				return true;
-			else 
+			else
 				return false;
 		} catch (SQLException e) {
 			Log.error(e.getMessage());
@@ -155,6 +119,64 @@ public class Database {
 			} catch (SQLException e) {
 				Log.error("Unable to close connection.");
 			}
+		}
+	}
+
+	private void update() {
+		TimedConnection c = null;
+		Statement s = null;
+		try {
+			c = cm.getConnection();
+			s = c.createStatement();
+
+			ResultSet res = s
+					.executeQuery("SELECT version FROM wc_info WHERE id=1");
+			byte version;
+			if(res.next())
+				version = res.getByte("version");
+			else 
+				version = 0;
+			
+			version++;
+			
+			switch (version) {
+			case 1:
+				s.execute("INSERT INTO wc_info (version) VALUES (\"1\")");
+				
+			case 2:
+				update2(c);
+
+				
+				s.execute("UPDATE wc_info SET version = \""+ current_version +"\"");
+			default:
+				break;
+			}
+		} catch (SQLException e) {
+			Log.error(e.getMessage());
+			plugin.disable("Can't reach database.");
+		} finally {
+			try {
+				if (s != null)
+					s.close();
+				c.release();
+			} catch (SQLException e) {
+				Log.error("Unable to close connection.");
+			}
+		}
+
+	}
+	
+	private void update2(TimedConnection c) throws SQLException{
+		Log.info("Updating database to version 2");
+		Statement s = c.createStatement();
+		s.execute("ALTER TABLE wc_players ADD nick char(36) NULL");
+		s = c.createStatement();
+		ResultSet res = s.executeQuery("SELECT id,uuid FROM wc_players");
+		while (res.next()){
+			Statement update = c.createStatement();
+			String nick = (Bukkit.getOfflinePlayer(UUID.fromString(res.getString("uuid")))
+					.getName()).toLowerCase();
+			update.execute("UPDATE wc_players SET nick = \""+nick+"\" WHERE id=\""+res.getInt("id")+"\"");
 		}
 		
 	}
